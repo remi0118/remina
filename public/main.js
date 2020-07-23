1bcff7d7fe4d4fde5f65f5f6693c6f1bbef80b98
YOSHIDA.Remina  <219c0449@s.obirin.ac.jp>
2020-07-24 02:29:00 +0900

    バグの修正

--- public/main.js
+++ public/main.js
@@ -8,23 +8,50 @@ const name = document.getElementById("name");
 
 let username='';
 nameform.addEventListener('submit', function(event){
-  const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
-  socketio.emit('message', msg);
+  if(name.value!==''){
+    username = name.value;
+    nameform.style.display ="none";
+    form.style.display ="block";
 
+    socketio.emit('signin');
+
+    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
+    socketio.emit('message', msg);
+  }
+
+  event.preventDefault();
 })
 
 form.addEventListener('submit', function(event){
-  const msg = {msg: input.value, name: username};
-  socketio.emit('message', msg);
-  input.value='';
+  if(input.value!==''){
+    const msg = {msg: input.value, name: username};
+    socketio.emit('message', msg);
+    input.value='';
+  }
   event.preventDefault();
 })
 
 socketio.on('message',function(msg){
+  if(username===''){
+    // まだ参加していなかったら、チャットを表示しない
+    return;
+  }
+  displayMessage(msg);
+});
+
+// 参加時に過去のメッセージを受け取る
+socketio.on('signin',function(msgs){
+  for(let i=0;i<msgs.length;i++){
+    const msg = msgs[i];
+    displayMessage(msg);
+  }
+});
+
+function displayMessage(msg){
   const dt = document.createElement("dt");
   const dd = document.createElement("dd");
   dt.append(msg.name);
   chats.append(dt);
   dd.append(msg.msg);
   chats.append(dd);
-});
+}
